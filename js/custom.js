c657c9a69d546393214a176853d3f0dd82316785
Sadman khan <developersadman@gmail.com>
2020-07-17 01:55:36 +0600

    Working on box section

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
     $(window).scroll(function () {
         var ws = $(window).scrollTop();
         $('.banner').css('background-position-y', (ws * 0.5));
+        $('.works').css('background-position-y', (ws - $('.works').offset().top) * 0.5)
     });
 });
